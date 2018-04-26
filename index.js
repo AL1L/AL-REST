7ac8cb10cfdb5f5e29e30a6080e1aba464ab7743
Allen Lantz <allen.m.lantz@gmail.com>
2018-04-25 22:56:36 -0500

    Remove dependencys and create entry point

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
- 
+import Server from "./src/server/server";
+import Log from "./src/server/log";
+import Request from "./src/server/request";
+import Response from "./src/server/response";
+import EndpointManager from "./src/server/endpoints/endpoint-manager";
+import EndpointLoader from "./src/server/endpoints/endpoint-loader";
+import Endpoint from "./src/server/endpoints/endpoint";
+
+module.exports = {
+    server: {
+        Server = Server,
+        Log: Log,
+        Request: Request,
+        Response: Response
+    },
+    endpoints: {
+        EndpointManager: EndpointManager,
+        EndpointLoader: EndpointLoader,
+        Endpoint: Endpoint
+    }
+}
