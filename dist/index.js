2d0ad69c65223945fa99b5b8c2ee9ce6a5fa18a7
Allen Lantz <allen.m.lantz@gmail.com>
2018-04-25 23:33:14 -0500

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,13 @@
 
 module.exports = {
     // Server
-    Server: require('./server/server'),
-    Log: require('./server/log'),
-    Request: require('./server/request'),
-    Response: require('./server/response'),
+    Server: require('./server/server').default,
+    Log: require('./server/log').default,
+    Request: require('./server/request').default,
+    Response: require('./server/response').default,
 
     // Endpoint
-    EndpointManager: require('./server/endpoints/endpoint-manager'),
-    EndpointLoader: require('./server/endpoints/endpoint-loader'),
-    Endpoint: require('./server/endpoints/endpoint')
+    EndpointManager: require('./server/endpoints/endpoint-manager').default,
+    EndpointLoader: require('./server/endpoints/endpoint-loader').default,
+    Endpoint: require('./server/endpoints/endpoint').default
 };
