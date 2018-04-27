c5015ba2acb1afeef40f3cc8e9bac8253520e784
Allen Lantz <allen.m.lantz@gmail.com>
2018-04-27 16:23:17 -0500

    Send endpoint errors to console.

--- src/server/endpoints/endpoint-manager.js
+++ src/server/endpoints/endpoint-manager.js
@@ -31,6 +31,8 @@ export default class EndpointManager {
                     await endpoint.handle(request);
                 } catch(e) {
                     request.res.setStatus(500, e);
+                    console.error(`Error while handleing endpoint ${endpoint._meta.name}`);
+                    console.error(e);
                     break;
                 }
             }
